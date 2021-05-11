ac6dd64e2c0cb2124c0e89a6dee5e3e1029364b0
Kastus' Kochubey <itnet.pf@yandex.ru>
2021-05-11 22:06:39 +0500

    init

---
+++ app/src/main/java/com/example/defmess/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.defmess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/defmess/MainFragment.java
@@ -0,0 +1,26 @@
+package com.example.defmess;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        super(R.layout.main_fragment);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
